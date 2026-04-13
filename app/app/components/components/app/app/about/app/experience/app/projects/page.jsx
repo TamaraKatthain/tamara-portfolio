df1d2f41997a6945f1160dcf862aa5d943148df6
TamaraKatthain <katthaint@gmail.com>
2026-04-13 13:25:43 -0700

    Create page.jsx

---
+++ app/app/components/components/app/app/about/app/experience/app/projects/page.jsx
@@ -0,0 +1,36 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1>Projects</h1>
+
+      <div style={{ marginTop: '20px' }}>
+        <h2>Writing</h2>
+
+        <p>
+          <a href="https://medium.com/@TamaraKatthain" target="_blank">
+            Medium
+          </a>
+        </p>
+
+        <p>
+          <a href="https://substack.com/@tamarakatthain" target="_blank">
+            Substack
+          </a>
+        </p>
+
+        <p>
+          <a href="https://www.uscannenbergmedia.com/people/tamara-katthain/" target="_blank">
+            USC News Columnist
+          </a>
+        </p>
+      </div>
+
+      <div style={{ marginTop: '40px' }}>
+        <h2>Advertising Projects</h2>
+
+        <p>P1 Skincare</p>
+        <p>Nike</p>
+      </div>
+    </section>
+  )
+}
