80e544df96f084e1e989fa622c3eff607e0cdf9e
TamaraKatthain <katthaint@gmail.com>
2026-04-13 13:26:10 -0700

    Create page.jsx

---
+++ app/app/components/components/app/app/about/app/experience/app/projects/app/contact/page.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <section>
+      <h1>Contact</h1>
+
+      <div style={{ marginTop: '20px' }}>
+        <p><strong>Email:</strong></p>
+        <a href="mailto:katthaint@gmail.com">
+          katthaint@gmail.com
+        </a>
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
+        <p><strong>LinkedIn:</strong></p>
+        <a href="https://www.linkedin.com/in/tamara-katthain" target="_blank">
+          www.linkedin.com/in/tamara-katthain
+        </a>
+      </div>
+    </section>
+  )
+}
