fe19f7c2229e84feadd04907bfc1be2ee0a79fad
TamaraKatthain <katthaint@gmail.com>
2026-04-13 13:23:11 -0700

    Create page.jsx

---
+++ app/app/components/components/app/app/about/page.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <section>
+      <h1>who really gets me:</h1>
+
+      <p style={{ marginTop: '20px' }}>
+        The kind of person who enjoys sitting down at a coffee shop, “people watching” for hours. They are obsessed with analyzing human behavior, especially consumer purchasing behavior, and finding patterns in what people like, what they buy, and what they are talking about. They are extremely open-minded, love learning about new cultures, and don’t shy away from new ideas. They inherently love to listen and believe that research and asking questions is the best way to fully understand people and the world around us.
+      </p>
+
+      <div style={{ marginTop: '40px' }}>
+        <h2>How I Want My Clients to Feel</h2>
+
+        <p style={{ marginTop: '10px' }}>
+          When I work with Tamara, it feels like we could brainstorm for hours. She makes me feel confident in speaking up and sharing my ideas in a judgment-free space. This environment we create makes our work so much better, as we deep dive and bounce ideas off each other, challenging one another’s views when needed without dismissing them. We analyze everything in depth, as we are both completely obsessed with analyzing human behavior and the underlying reasons behind our actions. Ultimately, I feel like she really cares about what I have to say, and that makes me want to show up with better, more reflective ideas.
+        </p>
+      </div>
+
+      <div style={{ marginTop: '40px' }}>
+        <h2>Fundamental Human Value</h2>
+        <p>Curiosity / Exploration</p>
+      </div>
+    </section>
+  )
+}
